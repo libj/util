dc070d21b42fa5a9059c3daf647d4e452c7744f3
Seva Safris <seva@safris.org>
2017-05-21 22:27:43 -0700

    Alter project structure

---
+++ src/test/java/org/safris/commons/util/IdentityArrayListTest.java
@@ -0,0 +1,11 @@
+package org.safris.commons.util;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class IdentityArrayListTest {
+  @Test
+  @Ignore("Implement me")
+  public void test() {
+  }
+}
