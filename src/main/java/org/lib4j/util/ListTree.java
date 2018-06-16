5fe1bfdd750d259b7be47b86b0b84d57c1ac68c7
Seva Safris <seva@safris.org>
2018-06-16 23:49:26 +0700

    HashTree -> ListTree

--- src/main/java/org/lib4j/util/ListTree.java
+++ src/main/java/org/lib4j/util/ListTree.java
@@ -1,15 +1,15 @@
 /* Copyright (c) 2006 lib4j
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * You should have received a copy of The MIT License (MIT) along with this
  * program. If not, see <http://opensource.org/licenses/MIT/>.
  */
@@ -19,12 +19,8 @@ package org.lib4j.util;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class HashTree<T> {
-  private List<Node<T>> children = null;
-
-  public void setChildren(final List<Node<T>> children) {
-    this.children = children;
-  }
+public class ListTree<T> {
+  private List<Node<T>> children;
 
   public List<Node<T>> getChildren() {
     return children;
@@ -45,9 +41,9 @@ public final class HashTree<T> {
     return children.get(index);
   }
 
-  public static final class Node<T> {
+  public static class Node<T> {
     private final T value;
-    private List<Node<T>> children = null;
+    private List<Node<T>> children;
 
     public Node(final T value) {
       this.value = value;
