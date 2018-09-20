798db8642cc4dcab663e3436c69e2decce78f338
Seva Safris <seva@safris.org>
2018-09-21 03:27:15 +0700

    Update DataEncoding API

---
+++ src/main/java/org/fastjax/util/DataEncoding.java
@@ -0,0 +1,41 @@
+/* Copyright (c) 2018 FastJAX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.fastjax.util;
+
+public abstract class DataEncoding<D,E> {
+  protected D data;
+  protected E encoded;
+
+  public DataEncoding(final D data, final E encoded) {
+    this.data = data;
+    this.encoded = encoded;
+  }
+
+  /**
+   * Returns the unencoded value.
+   *
+   * @return The unencoded value.
+   */
+  public abstract D getData();
+
+  /**
+   * Returns the encoded value.
+   *
+   * @return The encoded value.
+   */
+  public abstract E getEncoded();
+}
