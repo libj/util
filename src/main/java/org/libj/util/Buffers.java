14f22fc194ae30ed5e9028af8bb6a5387e1bca91
Seva Safris <seva@safris.org>
2019-11-11 12:46:49 +0800

    Implement method overloads for char, short and int.

--- src/main/java/org/libj/util/Buffers.java
+++ src/main/java/org/libj/util/Buffers.java
@@ -74,6 +74,132 @@ public final class Buffers {
    * <li>If {@code resize == 0}, do not resize the array. An
    * {@code ArrayIndexOutOfBoundsException} will be thrown.</li>
    * <li>If {@code resize > 0}, set the size of the array to the value of the
+   * index of the char that {@code bit} belongs to, plus 1, multiplied by
+   * {@code resize}:
+   * <blockquote><pre>(int)(((bit / 8) + 1) * resize)</pre></blockquote>
+   * <li>If {@code resize < 0}, set the size of the array to the value of the
+   * index of the char that {@code bit} belongs to, minus {@code (int)resize}:
+   * <blockquote><pre>(bit / 8) - (int)resize</pre></blockquote>
+   * </ul>
+   *
+   * @param buf The {@code char[]} buffer.
+   * @param bit The index of the bit to be set.
+   * @param resize The resize factor.
+   * @return The provided {@code char[]} buffer.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative,
+   *           or if {@code resize == 0}. and the bit index is greater than the
+   *           number of bits in the {@code buf} array.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static char[] set(char[] buf, final int bit, final double resize) {
+    final int i = bit / Character.SIZE;
+    if (i >= buf.length) {
+      if (resize == 0)
+        throw new ArrayIndexOutOfBoundsException(bit);
+
+      final char[] resized = new char[resize < 0 ? i - (int)resize : (int)((i + 1) * resize)];
+      System.arraycopy(buf, 0, resized, 0, buf.length);
+      buf = resized;
+    }
+
+    buf[i] |= 1 << (bit % Character.SIZE);
+    return buf;
+  }
+
+  /**
+   * Sets the bit at the specified index in {@code buf} to {@code 1}. If the bit
+   * index is greater than the number of bits in the {@code buf} array, the
+   * array will be resized based on the {@code resize} argument.
+   * <p>
+   * The {@code resize} argument behaves by the following rules:
+   * <ul>
+   * <li>If {@code resize == 0}, do not resize the array. An
+   * {@code ArrayIndexOutOfBoundsException} will be thrown.</li>
+   * <li>If {@code resize > 0}, set the size of the array to the value of the
+   * index of the short that {@code bit} belongs to, plus 1, multiplied by
+   * {@code resize}:
+   * <blockquote><pre>(int)(((bit / 8) + 1) * resize)</pre></blockquote>
+   * <li>If {@code resize < 0}, set the size of the array to the value of the
+   * index of the short that {@code bit} belongs to, minus {@code (int)resize}:
+   * <blockquote><pre>(bit / 8) - (int)resize</pre></blockquote>
+   * </ul>
+   *
+   * @param buf The {@code short[]} buffer.
+   * @param bit The index of the bit to be set.
+   * @param resize The resize factor.
+   * @return The provided {@code short[]} buffer.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative,
+   *           or if {@code resize == 0}. and the bit index is greater than the
+   *           number of bits in the {@code buf} array.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static short[] set(short[] buf, final int bit, final double resize) {
+    final int i = bit / Short.SIZE;
+    if (i >= buf.length) {
+      if (resize == 0)
+        throw new ArrayIndexOutOfBoundsException(bit);
+
+      final short[] resized = new short[resize < 0 ? i - (int)resize : (int)((i + 1) * resize)];
+      System.arraycopy(buf, 0, resized, 0, buf.length);
+      buf = resized;
+    }
+
+    buf[i] |= 1 << (bit % Short.SIZE);
+    return buf;
+  }
+
+  /**
+   * Sets the bit at the specified index in {@code buf} to {@code 1}. If the bit
+   * index is greater than the number of bits in the {@code buf} array, the
+   * array will be resized based on the {@code resize} argument.
+   * <p>
+   * The {@code resize} argument behaves by the following rules:
+   * <ul>
+   * <li>If {@code resize == 0}, do not resize the array. An
+   * {@code ArrayIndexOutOfBoundsException} will be thrown.</li>
+   * <li>If {@code resize > 0}, set the size of the array to the value of the
+   * index of the int that {@code bit} belongs to, plus 1, multiplied by
+   * {@code resize}:
+   * <blockquote><pre>(int)(((bit / 8) + 1) * resize)</pre></blockquote>
+   * <li>If {@code resize < 0}, set the size of the array to the value of the
+   * index of the int that {@code bit} belongs to, minus {@code (int)resize}:
+   * <blockquote><pre>(bit / 8) - (int)resize</pre></blockquote>
+   * </ul>
+   *
+   * @param buf The {@code int[]} buffer.
+   * @param bit The index of the bit to be set.
+   * @param resize The resize factor.
+   * @return The provided {@code int[]} buffer.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative,
+   *           or if {@code resize == 0}. and the bit index is greater than the
+   *           number of bits in the {@code buf} array.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static int[] set(int[] buf, final int bit, final double resize) {
+    final int i = bit / Integer.SIZE;
+    if (i >= buf.length) {
+      if (resize == 0)
+        throw new ArrayIndexOutOfBoundsException(bit);
+
+      final int[] resized = new int[resize < 0 ? i - (int)resize : (int)((i + 1) * resize)];
+      System.arraycopy(buf, 0, resized, 0, buf.length);
+      buf = resized;
+    }
+
+    buf[i] |= 1 << (bit % Integer.SIZE);
+    return buf;
+  }
+
+  /**
+   * Sets the bit at the specified index in {@code buf} to {@code 1}. If the bit
+   * index is greater than the number of bits in the {@code buf} array, the
+   * array will be resized based on the {@code resize} argument.
+   * <p>
+   * The {@code resize} argument behaves by the following rules:
+   * <ul>
+   * <li>If {@code resize == 0}, do not resize the array. An
+   * {@code ArrayIndexOutOfBoundsException} will be thrown.</li>
+   * <li>If {@code resize > 0}, set the size of the array to the value of the
    * index of the long that {@code bit} belongs to, plus 1, multiplied by
    * {@code resize}:
    * <blockquote><pre>(int)(((bit / 64) + 1) * resize)</pre></blockquote>
@@ -129,6 +255,63 @@ public final class Buffers {
    * Sets the bit in {@code buf} specified by the {@code bit} index to
    * {@code false}.
    *
+   * @param buf The {@code char[]} buffer in which to clear the bit.
+   * @param bit The index of the bit to be cleared.
+   * @return The provided {@code char[]} buffer.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static char[] clear(final char[] buf, final int bit) {
+    final int i = bit / Character.SIZE;
+    if (i >= buf.length)
+      return buf;
+
+    buf[bit / Character.SIZE] &= ~(1 << (bit % Character.SIZE));
+    return buf;
+  }
+
+  /**
+   * Sets the bit in {@code buf} specified by the {@code bit} index to
+   * {@code false}.
+   *
+   * @param buf The {@code short[]} buffer in which to clear the bit.
+   * @param bit The index of the bit to be cleared.
+   * @return The provided {@code short[]} buffer.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static short[] clear(final short[] buf, final int bit) {
+    final int i = bit / Short.SIZE;
+    if (i >= buf.length)
+      return buf;
+
+    buf[bit / Short.SIZE] &= ~(1 << (bit % Short.SIZE));
+    return buf;
+  }
+
+  /**
+   * Sets the bit in {@code buf} specified by the {@code bit} index to
+   * {@code false}.
+   *
+   * @param buf The {@code int[]} buffer in which to clear the bit.
+   * @param bit The index of the bit to be cleared.
+   * @return The provided {@code int[]} buffer.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static int[] clear(final int[] buf, final int bit) {
+    final int i = bit / Integer.SIZE;
+    if (i >= buf.length)
+      return buf;
+
+    buf[bit / Integer.SIZE] &= ~(1 << (bit % Integer.SIZE));
+    return buf;
+  }
+
+  /**
+   * Sets the bit in {@code buf} specified by the {@code bit} index to
+   * {@code false}.
+   *
    * @param buf The {@code long[]} buffer in which to clear the bit.
    * @param bit The index of the bit to be cleared.
    * @return The provided {@code long[]} buffer.
@@ -158,6 +341,54 @@ public final class Buffers {
   public static boolean get(final byte[] buf, final int bit) {
     final int i = bit / Byte.SIZE;
     return i < buf.length && ((buf[i] >> (bit % Byte.SIZE)) & 1) != 0;
+  }
+
+  /**
+   * Returns the value of the bit at the specified index. The value is
+   * {@code true} if the bit with the index {@code bit} is currently set in
+   * {@code buf}; otherwise, the result is {@code false}.
+   *
+   * @param buf The {@code char[]} buffer.
+   * @param bit The bit index.
+   * @return The value of the bit at the specified index.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static boolean get(final char[] buf, final int bit) {
+    final int i = bit / Character.SIZE;
+    return i < buf.length && ((buf[i] >> (bit % Character.SIZE)) & 1) != 0;
+  }
+
+  /**
+   * Returns the value of the bit at the specified index. The value is
+   * {@code true} if the bit with the index {@code bit} is currently set in
+   * {@code buf}; otherwise, the result is {@code false}.
+   *
+   * @param buf The {@code short[]} buffer.
+   * @param bit The bit index.
+   * @return The value of the bit at the specified index.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static boolean get(final short[] buf, final int bit) {
+    final int i = bit / Short.SIZE;
+    return i < buf.length && ((buf[i] >> (bit % Short.SIZE)) & 1) != 0;
+  }
+
+  /**
+   * Returns the value of the bit at the specified index. The value is
+   * {@code true} if the bit with the index {@code bit} is currently set in
+   * {@code buf}; otherwise, the result is {@code false}.
+   *
+   * @param buf The {@code int[]} buffer.
+   * @param bit The bit index.
+   * @return The value of the bit at the specified index.
+   * @throws ArrayIndexOutOfBoundsException If the specified index is negative.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static boolean get(final int[] buf, final int bit) {
+    final int i = bit / Integer.SIZE;
+    return i < buf.length && ((buf[i] >> (bit % Integer.SIZE)) & 1) != 0;
   }
 
   /**
@@ -195,6 +426,60 @@ public final class Buffers {
   }
 
   /**
+   * Returns the "logical size" of {@code buf}: the index of the char with the
+   * highest set bit in {@code buf}, plus one. Returns zero if {@code buf}
+   * contains no set bits.
+   *
+   * @param buf The {@code char[]} buffer.
+   * @return The logical size of the {@code buf}.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static int length(final char[] buf) {
+    if (buf.length == 0)
+      return 0;
+
+    int i = buf.length - 1;
+    while (buf[i] == 0 && --i >= 0);
+    return i + 1;
+  }
+
+  /**
+   * Returns the "logical size" of {@code buf}: the index of the short with the
+   * highest set bit in {@code buf}, plus one. Returns zero if {@code buf}
+   * contains no set bits.
+   *
+   * @param buf The {@code short[]} buffer.
+   * @return The logical size of the {@code buf}.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static int length(final short[] buf) {
+    if (buf.length == 0)
+      return 0;
+
+    int i = buf.length - 1;
+    while (buf[i] == 0 && --i >= 0);
+    return i + 1;
+  }
+
+  /**
+   * Returns the "logical size" of {@code buf}: the index of the int with the
+   * highest set bit in {@code buf}, plus one. Returns zero if {@code buf}
+   * contains no set bits.
+   *
+   * @param buf The {@code int[]} buffer.
+   * @return The logical size of the {@code buf}.
+   * @throws NullPointerException If the specified array is null.
+   */
+  public static int length(final int[] buf) {
+    if (buf.length == 0)
+      return 0;
+
+    int i = buf.length - 1;
+    while (buf[i] == 0 && --i >= 0);
+    return i + 1;
+  }
+
+  /**
    * Returns the "logical size" of {@code buf}: the index of the long with the
    * highest set bit in {@code buf}, plus one. Returns zero if {@code buf}
    * contains no set bits.
@@ -223,7 +508,7 @@ public final class Buffers {
    * </ul>
    *
    * @param buf The {@code byte[]} buffer.
-   * @return Returns a trimmed byte array of {@code buf}.
+   * @return A trimmed byte array of {@code buf}.
    * @throws NullPointerException If the specified array is null.
    * @see #length(byte[])
    */
@@ -233,6 +518,81 @@ public final class Buffers {
       return buf;
 
     final byte[] trimmed = new byte[length];
+    System.arraycopy(buf, 0, trimmed, 0, length);
+    return trimmed;
+  }
+
+  /**
+   * Returns a trimmed char array of {@code buf}: the length of a trimmed char
+   * array is its "logical size".
+   * <ul>
+   * <li>If the length of {@code buf} is its "logical size", the reference to
+   * {@code buf} is returned.</li>
+   * <li>If the length of {@code buf} is greater than its "logical size", a
+   * reference to a new char array is returned.</li>
+   * </ul>
+   *
+   * @param buf The {@code char[]} buffer.
+   * @return A trimmed char array of {@code buf}.
+   * @throws NullPointerException If the specified array is null.
+   * @see #length(char[])
+   */
+  public static char[] trimToLength(final char[] buf) {
+    final int length = length(buf);
+    if (buf.length == length)
+      return buf;
+
+    final char[] trimmed = new char[length];
+    System.arraycopy(buf, 0, trimmed, 0, length);
+    return trimmed;
+  }
+
+  /**
+   * Returns a trimmed short array of {@code buf}: the length of a trimmed short
+   * array is its "logical size".
+   * <ul>
+   * <li>If the length of {@code buf} is its "logical size", the reference to
+   * {@code buf} is returned.</li>
+   * <li>If the length of {@code buf} is greater than its "logical size", a
+   * reference to a new short array is returned.</li>
+   * </ul>
+   *
+   * @param buf The {@code short[]} buffer.
+   * @return A trimmed short array of {@code buf}.
+   * @throws NullPointerException If the specified array is null.
+   * @see #length(short[])
+   */
+  public static short[] trimToLength(final short[] buf) {
+    final int length = length(buf);
+    if (buf.length == length)
+      return buf;
+
+    final short[] trimmed = new short[length];
+    System.arraycopy(buf, 0, trimmed, 0, length);
+    return trimmed;
+  }
+
+  /**
+   * Returns a trimmed int array of {@code buf}: the length of a trimmed int
+   * array is its "logical size".
+   * <ul>
+   * <li>If the length of {@code buf} is its "logical size", the reference to
+   * {@code buf} is returned.</li>
+   * <li>If the length of {@code buf} is greater than its "logical size", a
+   * reference to a new int array is returned.</li>
+   * </ul>
+   *
+   * @param buf The {@code int[]} buffer.
+   * @return A trimmed int array of {@code buf}.
+   * @throws NullPointerException If the specified array is null.
+   * @see #length(int[])
+   */
+  public static int[] trimToLength(final int[] buf) {
+    final int length = length(buf);
+    if (buf.length == length)
+      return buf;
+
+    final int[] trimmed = new int[length];
     System.arraycopy(buf, 0, trimmed, 0, length);
     return trimmed;
   }
@@ -248,7 +608,7 @@ public final class Buffers {
    * </ul>
    *
    * @param buf The {@code long[]} buffer.
-   * @return Returns a trimmed long array of {@code buf}.
+   * @return A trimmed long array of {@code buf}.
    * @throws NullPointerException If the specified array is null.
    * @see #length(long[])
    */
@@ -293,6 +653,22 @@ public final class Buffers {
     final byte[] bytes = new byte[buf.length * Byte.SIZE];
     for (int i = 0; i < buf.length; ++i)
       toUnsignedString(buf[i], bytes, i, Byte.SIZE);
+
+    return new String(bytes);
+  }
+
+  /**
+   * Returns a string representing the big-endian binary representation of the
+   * specified char values.
+   *
+   * @param buf The char values.
+   * @return A string representing the big-endian binary representation of the
+   *         specified char values.
+   */
+  public static String toString(final char ... buf) {
+    final byte[] bytes = new byte[buf.length * Character.SIZE];
+    for (int i = 0; i < buf.length; ++i)
+      toUnsignedString(buf[i], bytes, i, Character.SIZE);
 
     return new String(bytes);
   }
