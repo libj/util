db95aadc5ba30e405f12f9ae19b60eaf78589d5d
Seva Safris <seva@safris.org>
2019-12-09 13:29:55 +0700

    Add AbstractIterator

---
+++ src/main/java/org/libj/util/AbstractIterator.java
@@ -0,0 +1,27 @@
+/* Copyright (c) 2019 LibJ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.libj.util;
+
+import java.util.Iterator;
+
+/**
+ * Abstract class form of the {@link Iterator} interface.
+ *
+ * @param <E> The type of elements returned by this iterator.
+ */
+public abstract class AbstractIterator<E> implements Iterator<E> {
+}
