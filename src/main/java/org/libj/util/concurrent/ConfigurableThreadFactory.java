43806c1880a1551e26ae8eac4370b4867a6f2fc6
Seva Safris <seva@safris.org>
2022-01-10 16:23:49 +0700

    newThreadFunction

--- src/main/java/org/libj/util/concurrent/ConfigurableThreadFactory.java
+++ src/main/java/org/libj/util/concurrent/ConfigurableThreadFactory.java
@@ -18,13 +18,15 @@ package org.libj.util.concurrent;
 
 import static org.libj.lang.Assertions.*;
 
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.libj.util.function.TriObjLongFunction;
+
 /**
- * A {@link ThreadFactory} that provides a constructor that allows configuration
- * of parameters to be applied to new {@linkplain Thread threads} returned by
- * {@link #newThread(Runnable)}.
+ * A {@link ThreadFactory} that provides a constructor that allows configuration of parameters to be applied to new
+ * {@linkplain Thread threads} returned by {@link #newThread(Runnable)}.
  */
 public class ConfigurableThreadFactory implements ThreadFactory {
   private static final AtomicLong threadFactorySeq = new AtomicLong(1);
@@ -35,6 +37,8 @@ public class ConfigurableThreadFactory implements ThreadFactory {
   protected final boolean daemon;
   protected final int priority;
   protected final ThreadGroup group;
+  protected final UncaughtExceptionHandler handler;
+  protected final TriObjLongFunction<ThreadGroup,Runnable,String,Thread> newThreadFunction;
 
   /**
    * Created a new {@link ConfigurableThreadFactory} with the provided arguments.
@@ -44,23 +48,36 @@ public class ConfigurableThreadFactory implements ThreadFactory {
    * @param stackSize The stack size to be used for new threads.
    * @param daemon The daemon boolean to be used for new threads.
    * @param priority The priority to be used for new threads.
-   * @throws IllegalArgumentException If {@code stackSize} is negative, or if
-   *           {@code priority} is outside the range of
+   * @param handler The handler to be invoked when a thread is abruptly terminated due to an uncaught exception.
+   * @param newThreadFunction The function to be called for the creation of new {@link Thread}s.
+   * @throws IllegalArgumentException If {@code stackSize} is negative, or if {@code priority} is outside the range of
    *           {@link Thread#MIN_PRIORITY} and {@link Thread#MAX_PRIORITY}.
    */
-  public ConfigurableThreadFactory(final ThreadGroup group, final String namePrefix, final long stackSize, final boolean daemon, final int priority) {
+  public ConfigurableThreadFactory(final ThreadGroup group, final String namePrefix, final long stackSize, final boolean daemon, final int priority, final UncaughtExceptionHandler handler, final TriObjLongFunction<ThreadGroup,Runnable,String,Thread> newThreadFunction) {
     final SecurityManager s;
     this.group = group != null ? group : (s = System.getSecurityManager()) != null ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
     this.namePrefix = (namePrefix != null ? namePrefix : getClass().getSimpleName()) + "-" + threadFactorySeq.getAndIncrement() + "-";
     this.stackSize = assertNotNegative(stackSize);
     this.daemon = daemon;
     this.priority = assertRangeMinMax(priority, Thread.MIN_PRIORITY, Thread.MAX_PRIORITY);
+    this.handler = handler;
+    this.newThreadFunction = newThreadFunction;
   }
 
   @Override
   public Thread newThread(final Runnable r) {
-    final Thread thread = new Thread(group, r, namePrefix + threadSeq.getAndIncrement(), stackSize);
-    thread.setDaemon(daemon);
+    final String name = namePrefix + threadSeq.getAndIncrement();
+    final Thread thread = newThreadFunction != null ? newThreadFunction.apply(group, r, name, stackSize) : new Thread(group, r, name, stackSize);
+
+    if (thread.isDaemon() != daemon)
+      thread.setDaemon(daemon);
+
+    if (group != null)
+      thread.setUncaughtExceptionHandler(group);
+
+    if (handler != null)
+      thread.setUncaughtExceptionHandler(handler);
+
     if (thread.getPriority() != priority)
       thread.setPriority(priority);
 
--- src/main/java/org/libj/util/concurrent/ThreadFactoryBuilder.java
+++ src/main/java/org/libj/util/concurrent/ThreadFactoryBuilder.java
@@ -15,24 +15,23 @@
  */
 
 package org.libj.util.concurrent;
+
 import static org.libj.lang.Assertions.*;
 
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.concurrent.ThreadFactory;
 
 import org.libj.util.function.TriObjLongFunction;
 
 /**
- * A builder that produces {@link ThreadFactory} instances given provided
- * options.
+ * A builder that produces {@link ThreadFactory} instances given provided options.
  */
 public class ThreadFactoryBuilder {
   private ThreadGroup group;
 
   /**
-   * Sets the {@link ThreadGroup} to be provided to the {@link ThreadFactory}
-   * produced by {@link #build()}, which is thereafter provided to the
-   * {@link Thread} instances produced by
-   * {@link ThreadFactory#newThread(Runnable)}.
+   * Sets the {@link ThreadGroup} to be provided to the {@link ThreadFactory} produced by {@link #build()}, which is thereafter
+   * provided to the {@link Thread} instances produced by {@link ThreadFactory#newThread(Runnable)}.
    *
    * @param group The {@link ThreadGroup}.
    * @return {@code this} {@link ThreadFactoryBuilder}.
@@ -45,9 +44,8 @@ public class ThreadFactoryBuilder {
   private String namePrefix;
 
   /**
-   * Sets the name prefix to be provided to the {@link ThreadFactory} produced
-   * by {@link #build()}, which is thereafter provided to the {@link Thread}
-   * instances produced by {@link ThreadFactory#newThread(Runnable)}.
+   * Sets the name prefix to be provided to the {@link ThreadFactory} produced by {@link #build()}, which is thereafter provided to
+   * the {@link Thread} instances produced by {@link ThreadFactory#newThread(Runnable)}.
    *
    * @param namePrefix The name prefix.
    * @return {@code this} {@link ThreadFactoryBuilder}.
@@ -60,10 +58,8 @@ public class ThreadFactoryBuilder {
   private int stackSize;
 
   /**
-   * Sets the stack size to be provided to the {@link ThreadFactory}
-   * produced by {@link #build()}, which is thereafter provided to the
-   * {@link Thread} instances produced by
-   * {@link ThreadFactory#newThread(Runnable)}.
+   * Sets the stack size to be provided to the {@link ThreadFactory} produced by {@link #build()}, which is thereafter provided to
+   * the {@link Thread} instances produced by {@link ThreadFactory#newThread(Runnable)}.
    *
    * @param stackSize The stack size.
    * @return {@code this} {@link ThreadFactoryBuilder}.
@@ -77,10 +73,8 @@ public class ThreadFactoryBuilder {
   private boolean daemon;
 
   /**
-   * Sets the daemon boolean to be provided to the {@link ThreadFactory}
-   * produced by {@link #build()}, which is thereafter provided to the
-   * {@link Thread} instances produced by
-   * {@link ThreadFactory#newThread(Runnable)}.
+   * Sets the daemon boolean to be provided to the {@link ThreadFactory} produced by {@link #build()}, which is thereafter provided
+   * to the {@link Thread} instances produced by {@link ThreadFactory#newThread(Runnable)}.
    *
    * @param daemon The daemon boolean.
    * @return {@code this} {@link ThreadFactoryBuilder}.
@@ -93,23 +87,41 @@ public class ThreadFactoryBuilder {
   private int priority = Thread.NORM_PRIORITY;
 
   /**
-   * Sets the priority to be provided to the {@link ThreadFactory} produced by
-   * {@link #build()}, which is thereafter provided to the {@link Thread}
-   * instances produced by {@link ThreadFactory#newThread(Runnable)}.
+   * Sets the priority to be provided to the {@link ThreadFactory} produced by {@link #build()}, which is thereafter provided to the
+   * {@link Thread} instances produced by {@link ThreadFactory#newThread(Runnable)}.
    *
    * @param priority The priority.
    * @return {@code this} {@link ThreadFactoryBuilder}.
-   * @throws IllegalArgumentException If {@code priority} is outside the range
-   *           of {@link Thread#MIN_PRIORITY} and {@link Thread#MAX_PRIORITY}.
+   * @throws IllegalArgumentException If {@code priority} is outside the range of {@link Thread#MIN_PRIORITY} and
+   *           {@link Thread#MAX_PRIORITY}.
    */
   public ThreadFactoryBuilder withPriority(final int priority) {
     this.priority = assertRangeMinMax(priority, Thread.MIN_PRIORITY, Thread.MAX_PRIORITY);
     return this;
   }
 
+  private UncaughtExceptionHandler handler;
+
+  /**
+   * Set the handler to be invoked when a thread is abruptly terminated due to an uncaught exception.
+   *
+   * @param handler The handler to be invoked when a thread is abruptly terminated due to an uncaught exception.
+   * @return {@code this} {@link ThreadFactoryBuilder}.
+   */
+  public ThreadFactoryBuilder withUncaughtExceptionHandler(final UncaughtExceptionHandler handler) {
+    this.handler = handler;
+    return this;
+  }
+
   private TriObjLongFunction<ThreadGroup,Runnable,String,Thread> newThreadFunction;
 
-  public ThreadFactoryBuilder withWewThread(final TriObjLongFunction<ThreadGroup,Runnable,String,Thread> newThreadFunction) {
+  /**
+   * Sets the function to be called for the creation of new {@link Thread}s.
+   *
+   * @param newThreadFunction The function to be called for the creation of new {@link Thread}s.
+   * @return {@code this} {@link ThreadFactoryBuilder}.
+   */
+  public ThreadFactoryBuilder withNewThread(final TriObjLongFunction<ThreadGroup,Runnable,String,Thread> newThreadFunction) {
     this.newThreadFunction = newThreadFunction;
     return this;
   }
@@ -120,14 +132,6 @@ public class ThreadFactoryBuilder {
    * @return A new {@link ThreadFactory} given the provided options.
    */
   public ThreadFactory build() {
-    if (newThreadFunction == null)
-      return new ConfigurableThreadFactory(group, namePrefix, stackSize, daemon, priority);
-
-    return new ConfigurableThreadFactory(group, namePrefix, stackSize, daemon, priority) {
-      @Override
-      public Thread newThread(final Runnable r) {
-        return newThreadFunction.apply(group, r, namePrefix, stackSize);
-      }
-    };
+    return new ConfigurableThreadFactory(group, namePrefix, stackSize, daemon, priority, handler, newThreadFunction);
   }
 }
